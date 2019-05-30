fd0e355d8a3dfb6fe9a509fa5ca33f5406c0b975
xuliam <lian.xu77@gmail.com>
2019-05-30 22:00:06 +1200

    small bug

---
+++ Practice_Start_Week6/src/Practice_Start_Week6/loginFrame.java
@@ -0,0 +1,43 @@
+package Practice_Start_Week6;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import com.sun.tools.javac.launcher.Main;
+
+public class loginFrame extends AbstractFrame{
+
+	/**
+	 * TODO
+	 * @author xulian
+	 * @VERSION 1.0
+	 * @DATE 30/05/2019 9:58:35 pm
+	 * @remarks TODO
+	 *
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+	public loginFrame(int width, int height) {
+		super(width, height);
+		initComponents();
+	}
+
+	private void initComponents() {
+		JPanel contentPane = new JPanel();
+		contentPane.setLayout(null);
+		
+		JLabel lblLoginName = new JLabel("User Name:");
+		lblLoginName.setBounds(65, 278, 80, 25);
+		JLabel lblLoginPwd = new JLabel("Code No. :");
+		lblLoginPwd.setBounds(lblLoginPwd.getX(), lblLoginName.getY()+30, lblLoginName.getWidth(), lblLoginName.getHeight());
+		
+		contentPane.add(lblLoginName);
+		contentPane.add(lblLoginPwd);
+		
+		setContentPane(contentPane);
+	
+	}
+	public static void main(String[] args) {
+		 new loginFrame(642,491).setVisible(true);
+	}
+}
